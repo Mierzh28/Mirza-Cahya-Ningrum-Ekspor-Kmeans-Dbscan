0a7bba1e374670cff79bd0ca6b75d1d22a08b5ec
Mierzh28 <mirzaningrum14@gmail.com>
2025-12-02 07:33:29 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1u-UPIdbLQOPyXW3lEMtKkLPGXn_rTy_S
 """
 
-!pip install streamlit
 !pip install pyngrok
 !pip install openpyxl
 
